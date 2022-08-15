4fc166c6ac667e27e7737228055d3542d07f97dc
Jacob <jacobsimoninielsen@gmail.com>
2022-08-15 14:01:51 +0200

    Console Log First edition

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const h1 = document.getElementsByTagName("h1")[0].innerHTML;
+const h2 = document.getElementsByClassName("smallHeadline")[0].innerHTML;
+const h2Two = document.getElementsByClassName("smallHeadline")[1].innerHTML;
+const para = document.getElementsByClassName("text")[0].innerHTML;
+const fieldset = document.getElementById("fieldset");
+const legend = document.getElementsByClassName("legendText")[0].innerHTML;
+
+console.log(h1);
+console.log(h2,h2Two);
+console.log(para);
+console.log(fieldset);
+console.log(legend);
