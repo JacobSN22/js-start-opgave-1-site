376ef6f4c03d26747b904ca46980e626ae93e524
Jacob <jacobsimoninielsen@gmail.com>
2022-08-15 13:12:44 +0200

    Dom Markup
    Givet ID/Class til alle elementerne der skulle have det.

--- js/variables.js
+++ js/variables.js
@@ -10,7 +10,13 @@ const h = undefined;
 const i = function () { };
 const j = new Date();
 
-
-
-
-
+console.log("Variabel a er: "+ typeof(a));
+console.log("Variabel b er: "+ typeof(b));
+console.log("Variabel c er: "+ typeof(c));
+console.log("Variabel d er: "+ typeof(d));
+console.log("Variabel e er: "+ typeof(e));
+console.log("Variabel f er: "+ typeof(f));
+console.log("Variabel g er: "+ typeof(g));
+console.log("Variabel h er: "+ typeof(h));
+console.log("Variabel i er: "+ typeof(i));
+console.log("Variabel j er: "+ typeof(j));
