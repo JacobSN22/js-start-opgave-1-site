065f372f6668ff31debc90a73c790f4840ca6879
Jacob <jacobsimoninielsen@gmail.com>
2022-08-16 11:07:03 +0200

    Upated version (Done)

--- js/index.js
+++ js/index.js
@@ -4,9 +4,68 @@ const h2Two = document.getElementsByClassName("smallHeadline")[1].innerHTML;
 const para = document.getElementsByClassName("text")[0].innerHTML;
 const fieldset = document.getElementById("fieldset");
 const legend = document.getElementsByClassName("legendText")[0].innerHTML;
+const month = document.getElementsByClassName("month")[0].innerHTML;
+const inptMonth = document.getElementsByClassName("inptMonth");
+const check = document.getElementsByClassName("check")[0].innerHTML;
+const checkbox = document.getElementsByClassName("checkbox")
+const color = document.getElementsByClassName("color")[0].innerHTML;
+const inptColor = document.getElementsByClassName("inptColor");
+const radio = document.getElementsByClassName("radio")[0].innerHTML;
+const radioOne = document.getElementsByClassName("radioOne")
+const rInptOne = document.getElementsByClassName("rInptOne")
+const radioTwo = document.getElementsByClassName("radioTwo")
+const rInptTwo = document.getElementsByClassName("rInptTwo")
+const radioThree = document.getElementsByClassName("radioThree")
+const rInptThree = document.getElementsByClassName("rInptThree")
+const range = document.getElementsByClassName("range")[0].innerHTML;
+const inptRange = document.getElementsByClassName("inptRange")
+const inptButton = document.getElementsByClassName("inptButton")
+const inptReset = document.getElementsByClassName("inptReset")
+const inptSubmit = document.getElementsByClassName("inptSubmit")
+const button = document.getElementById("button").innerHTML;
+const selectLabel = document.getElementsByClassName("select")[0].innerHTML;
+const selectGroup = document.getElementById("select")
+const optGroupOne = document.getElementById("optOne")
+const otpOne = document.getElementsByClassName("optOne")[0].innerHTML;
+const optTwo = document.getElementsByClassName("optTwo")[0].innerHTML;
+const optThree = document.getElementsByClassName("optThree")[0].innerHTML;
+const optGroupTwo = document.getElementById("optTwo")
+const optFour = document.getElementsByClassName("optFour")[0].innerHTML;
+const optFive = document.getElementsByClassName("optFive")[0].innerHTML;
+const optSix = document.getElementsByClassName("optSix")[0].innerHTML;
 
 console.log(h1);
-console.log(h2,h2Two);
+console.log(h2);
 console.log(para);
+console.log(h2Two);
 console.log(fieldset);
 console.log(legend);
+console.log(month);
+console.log(inptMonth);
+console.log(check);
+console.log(checkbox);
+console.log(color);
+console.log(inptColor);
+console.log(radio);
+console.log(radioOne);
+console.log(rInptOne);
+console.log(radioTwo);
+console.log(rInptTwo);
+console.log(radioThree);
+console.log(rInptThree);
+console.log(range);
+console.log(inptRange);
+console.log(inptButton);
+console.log(inptReset);
+console.log(inptSubmit);
+console.log(button);
+console.log(selectLabel);
+console.log(selectGroup);
+console.log(optGroupOne);
+console.log(otpOne);
+console.log(optTwo);
+console.log(optThree);
+console.log(optGroupTwo);
+console.log(optFour);
+console.log(optFive);
+console.log(optSix);
